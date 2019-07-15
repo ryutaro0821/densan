2f60e4edaa22601ab5ce368218ee27a050a46f45
ryutaro0821 <ryutaro0821.1163@gmail.com>
2019-07-15 18:00:23 +0900

    スクリーンクラス

---
+++ game/densan/screen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace densan
+{
+    class screen
+    {
+        public static readonly int Width = 800;
+        public static readonly int Height = 480;
+    }
+}
